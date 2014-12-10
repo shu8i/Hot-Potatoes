7fe2d84c65550350e6cf46298264640c6023f8ab
zguadagn <Zac@zacharys-mbp.fios-router.home>
2014-12-10 13:59:42 -0500

    Help Button & Wall-E Rotate
    Added a help button to the login panel and I rotated Wall-E so he faces
    the direction he will move in

--- src/view/LoginPanel.java
+++ src/view/LoginPanel.java
@@ -5,6 +5,8 @@ import util.Constants;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -44,6 +46,50 @@ public class LoginPanel extends JPanel
         final HintPanel hintPanel = new HintPanel(new Dimension(250, 20));
 
         JButton loginButton = new JButton("Login");
+        
+        JButton helpButton = new JButton("Help");
+        
+        helpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame help = new JFrame("Help");
+                help.setResizable(false);
+                JPanel panel = new JPanel();
+            	help.setVisible(true);
+            	help.setSize(600, 600);
+                help.add(panel);
+                JTextArea helpText = new JTextArea();
+                helpText.setLineWrap(true);
+                helpText.setWrapStyleWord(true);
+                helpText.setText("For Admins: There are three options for admin activity. The registration button (the middle button) "
+                		+ "allows a teacher to add new teachers to the program as admins and add new students to the program as players. "
+                		+ "Additionally, teachers can delete students and teachers from the program here. The left button is the highscore "
+                		+ "button which is used to track student progress in each level. Finally, the right button is the world edit button. "
+                		+ "This is where teachers will spend a bulk of their time on this program editing new worlds for studets to play. "
+                		+ "Each world must have only one Karel and only one home and have a grid size from 10 to 50. Everything else is up to "
+                		+ "the teacher. Used to check fields at the bottom to select which item to place in the play grid and click to place "
+                		+ "the item. Double clicking will delete an item and clicking over an object will replace it with the selected object. "
+                		+ "When a teacher is done, he can save the world using the menu on the top left area of the play window. Here a teacher can "
+                		+ "also load a world to further edit it or even delete a world.												"
+                		+ "For Students: Students can select whichever world they wish to play upon logging in. Upon loading the world, "
+                		+ "students will see a play grid, a blank side panel next to the grid, and buttons on the bottom of the play window. "
+                		+ "The main objective for the student is to make programs for Wall-E to collect all the potatoes and reach the home on the grid. "
+                		+ "A student may do this by clicking on the buttons they wish to add to his program. Once a button is clicked, "
+                		+ "it will be added to the side panel next to the grid. A student may delete a piece of code by double clicking it "
+                		+ "and he may also add after a specific piece of code by clicking on the piece of codes he wants to add after "
+                		+ "and then click the code to be added itself. The buttons on the bottom will change depending on what a student's valid "
+                		+ "options to add to a program are. For example, all IF and WHILE code pieces must end with an END. A student may save "
+                		+ "a created program by using the save as macro option in the menu that is at the top of the play window. Once a macro is save, "
+                		+ "it will appear in the macro area at the bottom right of the play window. A student may also use this menu to clear all "
+                		+ " of his code from the side panel, run his code on the world, go back to select a new world, or run his code in "
+                		+ "step by step mode. Here a student will be able to see exactly what his code does. Once a student completes a world, "
+                		+ "a message will be displayed at the bottom to notify the student with his score and send him back to the "
+                		+ "world selection screen.");
+                helpText.setPreferredSize(new Dimension(600, 600));
+                helpText.setEditable(false);
+                panel.add(helpText);
+            }
+        });
 
         loginButton.addActionListener(new ActionListener() {
             @Override
@@ -142,6 +188,13 @@ public class LoginPanel extends JPanel
         c.fill = GridBagConstraints.HORIZONTAL;
         loginButton.setFocusable(false);
         add(loginButton, c);
+        
+        c = new GridBagConstraints();
+        c.gridx = 25;
+        c.gridy = 3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        helpButton.setFocusable(false);
+        add(helpButton, c);
 
     }
 
